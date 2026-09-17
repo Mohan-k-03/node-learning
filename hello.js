7dd20a0ff19df93cb2bd622921021abe32b2997c
Mohan k <mohanarul2003@gmail.com>
2026-09-17 20:34:00 +0530

    .

--- hello.js
+++ hello.js
@@ -1,1 +1,16 @@
 console.log("Hello Mohan");
+
+console.log(global);
+
+setTimeout(() => {
+  console.log("hello");
+  clearInterval(clear);
+}, 5000);
+
+const clear = setInterval(() => {
+  console.log("super");
+}, 1000);
+
+
+console.log(__dirname);
+console.log(__filename)
