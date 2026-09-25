f4425b2aa3ca5865eea52d2ab1536913bf8652bf
Mohan k <mohanarul2003@gmail.com>
2026-09-25 17:46:09 +0530

    middleware and thirdparty middleware

--- App.js
+++ App.js
@@ -1,8 +1,25 @@
 const expresss = require("express");
+const morgan = require("morgan");
 
 const app = expresss();
 
 app.listen(3000);
+
+// app.use((req, res, next) => {
+//   console.log("this is midddle ware,request received");
+//   console.log(req.host);
+//   console.log(req.path);
+//   console.log(req.method);
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   console.log("middleware 1");
+//   next();
+// });
+
+// app.use(morgan("dev"));
+app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
   //   res.status(200).send('<h1>codeio</h1>');
@@ -15,13 +32,16 @@ app.get("/join", (req, res) => {
 app.get("/about", (req, res) => {
   res.sendFile("./docs/about.html", { root: __dirname });
 });
-app.get('/joinus',(req,res)=>{
-    res.redirect('/join')
-})
 
-app.use((req,res) => {
-  res.status(404).sendFile("./docs/Error_404.svg", { root: __dirname });
+// app.use((req, res, next) => {
+//   console.log("middleware 2");
+//   next();
+// });
+
+app.get("/joinus", (req, res) => {
+  res.redirect("/join");
 });
 
-
-            
+app.use((req, res) => {
+  res.status(404).sendFile("./docs/Error_404.svg", { root: __dirname });
+});
